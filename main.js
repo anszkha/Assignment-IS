f6f6c3848a7f848f702725b4ebecc1a107c6b52e
anszkha <anszkha0609@gmail.com>
2023-12-27 08:13:36 +0800

    messages

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const Staff = require("./staff");
 const Visitor = require("./visitor");
 
 MongoClient.connect(
-	// TODO: Connection  yAs8O9sckieZkYeg
+	// TODO: Connection yAs8O9sckieZkYeg
 	"mongodb://anis:yAs8O9sckieZkYeg@ac-nlxl7jy-shard-00-00.glzpa13.mongodb.net:27017,ac-nlxl7jy-shard-00-01.glzpa13.mongodb.net:27017,ac-nlxl7jy-shard-00-02.glzpa13.mongodb.net:27017/?replicaSet=atlas-es7nad-shard-0&ssl=true&authSource=admin",
 	{ useNewUrlParser: true },
 ).catch(err => {
